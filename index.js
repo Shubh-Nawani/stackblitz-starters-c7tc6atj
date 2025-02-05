b913ccb7e8d1a7e6a12c867bd5e24252ab22a2c4
Shubh Nawani <shubhnawani@outlook.com>
2025-02-05 15:36:07 +0530

    Completed the Assignment

--- index.js
+++ index.js
@@ -1,15 +1,58 @@
 const express = require('express');
+const connectDB = require('./config/db')
 const { resolve } = require('path');
-
+const User = require('./schema')
+require('dotenv').config()
 const app = express();
+app.use(express.json())
+
+
 const port = 3010;
 
 app.use(express.static('static'));
+
+app.post("/api/users", async (req, res) => {
+  try {
+    const {name, email, password, createdAt} = req.body
+    
+    if (name == null || email == null || password == null) {
+      return res.status(405).send("Provide required fields!")
+    }
+
+    const existingUser = await User.findOne({email})
+
+    if (existingUser) {
+      return res.status(400).send("User already exists")
+    }
+
+    const newUser = new User({
+      name,
+      email,
+      password,
+      createdAt
+    })
+
+    await newUser.save()
+    return res.status(201).send(newUser)
+
+
+
+    
+  } catch (err) {
+    return res.status(500).send(err.message)
+  }
+})
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  try {
+    connectDB()
+    console.log(`Example app listening at http://localhost:${port}`);
+  } catch (err) {
+    console.error(err.message)
+  }
+  
 });
